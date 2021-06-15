9f304a394045c68a2800974db46fa384161ab44d
Derek Zhou <derek@3qin.us>
2021-06-15 18:28:16 +0000

    add some missing tags

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HtmlWriter.MixProject do
   def project do
     [
       app: :html_writer,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
